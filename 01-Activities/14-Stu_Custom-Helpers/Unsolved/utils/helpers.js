197711548a20e96ca994d671206edaea0f1d8be2
Taylor Gonzales <hello@taylorgonz.com>
2021-03-20 09:47:45 -0400

    wednesday activities

--- 01-Activities/14-Stu_Custom-Helpers/Unsolved/utils/helpers.js
+++ 01-Activities/14-Stu_Custom-Helpers/Unsolved/utils/helpers.js
@@ -3,5 +3,11 @@ module.exports = {
     return date.toLocaleTimeString();
   },
   // TODO: Create a custom helper 'format_date' that takes in a timestamp,
+  format_date: (date) => {
+    return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${
+      new Date(date).getFullYear() + 5
+    }`
+  },
   // adds five years to the date, and formats it as M/D/YYYY
+  
 };
